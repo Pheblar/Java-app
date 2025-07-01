146ae07cd9cdd52ea7525e09633049c4b293df00
Endesapt <103508859+Endesapt@users.noreply.github.com>
2025-07-01 12:13:34 +0300

    remove ports and url from react app

--- front-end/src/api/api-connector.jsx
+++ front-end/src/api/api-connector.jsx
@@ -1,10 +1,7 @@
 import Axios from "axios";
 
-const apiUrl = process.env.REACT_APP_API_URL;
-const apiPort = process.env.REACT_APP_API_PORT;
-
 export async function getPostData(){
-    return await Axios.get(`${apiUrl}:${apiPort}/api/v1/posts`)
+    return await Axios.get(`/api/v1/posts`)
         .then(res => {
             console.log("getPostData"+res.data);
             return res.data
@@ -13,7 +10,7 @@ export async function getPostData(){
 }
 
 export async function savePost(postData){
-    return await Axios.post(`${apiUrl}:${apiPort}/api/v1/create-post`, postData,
+    return await Axios.post(`/api/v1/create-post`, postData,
         {
             headers: {
                 'Content-Type': 'application/json'
